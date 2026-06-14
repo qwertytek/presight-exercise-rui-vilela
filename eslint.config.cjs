8e58fb32e736ebdb85f86f4e8dcaeb6d5f69259d
Rui Vilela <tek@qwertytek.com>
2026-06-14 23:44:21 +0100

    chore(eslint): update config for server, client, base and ignore rules

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,11 +1,72 @@
-module.exports = {
-  env: {
-    node: true,
-    es2022: true,
+const js = require('@eslint/js');
+const globals = require('globals');
+const tseslint = require('typescript-eslint');
+
+module.exports = [
+  // IGNORES
+  {
+    ignores: [
+      '**/node_modules/',
+      '**/dist/',
+      '**/build/',
+      '**/coverage/',
+      '**/*.min.js',
+      '.config/*',
+      'eslint.config.cjs'
+    ],
   },
-  extends: ['eslint:recommended'],
-  rules: {
-    'no-console': 'warn',
-    'no-debugger': 'error',
+  // BASE CONFIG
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./client/tsconfig.json', './server/tsconfig.json'],
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+    },
   },
-};
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  // CLIENT SPECIFIC CONFIGS
+  {
+    files: ['client/**/*.js', 'client/**/*.ts', 'client/**/*.jsx', 'client/**/*.tsx'],
+    languageOptions: {
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './client/tsconfig.json',
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  // SERVER SPECIFIC CONFIGS
+  {
+    files: ['server/**/*.js', 'server/**/*.ts'],
+    languageOptions: {
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './server/tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
