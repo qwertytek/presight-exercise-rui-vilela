509fcd310a4614dfb5ea693ec9574473fd2dfa94
Rui Vilela <tek@qwertytek.com>
2026-06-15 03:04:15 +0100

    feat(db): define database schema

--- server/src/db/migrations/001_init.sql
+++ server/src/db/migrations/001_init.sql
@@ -1,7 +1,42 @@
+-- ====================================================================
+-- INDEPENDENT TABLES
+-- ====================================================================
+
+CREATE TABLE nationality (
+    code VARCHAR(2) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE hobby (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    type VARCHAR(100) NOT NULL
+);
+
+-- ====================================================================
+--  USER TABLE
+-- ====================================================================
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     avatar TEXT,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    age INTEGER
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    age INTEGER,
+    nationality VARCHAR(2),
+    FOREIGN KEY (nationality) REFERENCES nationality(code)
+);
+
+-- ====================================================================
+-- JUNCTION TABLE (Many-to-Many)
+-- ====================================================================
+
+CREATE TABLE user_hobby (
+    user_id INTEGER,
+    hobby_id INTEGER,
+
+    PRIMARY KEY (user_id, hobby_id),
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (hobby_id) REFERENCES hobby(id) ON DELETE CASCADE
 );
