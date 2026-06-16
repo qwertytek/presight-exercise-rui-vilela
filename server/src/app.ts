03b53658f8b70d2e944e08ec13f206842450f63e
Rui Vilela <tek@qwertytek.com>
2026-06-16 22:52:58 +0100

    chore(server): add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import cors from 'cors';
 import usersRouter from './module/users/users.router';
 
 /* API ROUTER */
@@ -8,11 +9,20 @@ apiRouter.use('/users', usersRouter);
 /* TODO: if it gets to big move API ROUTER to it's own file */
 
 const app = express();
+app.use(
+  cors({
+    /* in production replace with env variables */
+    origin: ['http://localhost:5173', 'http://192.168.160.2:5173/'],
+    credentials: true,
+  }),
+);
+
+app.use('/api', apiRouter);
+
+app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('Hello World');
 });
-
-app.use('/api', apiRouter);
 
 export default app;
